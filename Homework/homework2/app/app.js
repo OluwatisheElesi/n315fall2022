3c559083ad405dc7c3f69d731436fa0502fc256a
OluwatisheElesi <tisheelesh.logic@gmail.com>
2022-09-22 10:29:11 -0400

    3

--- Homework/homework2/app/app.js
+++ Homework/homework2/app/app.js
@@ -13,33 +13,33 @@ function initListeners() {
     });
 
 
-//     const userCardTemplate = document.querySelector("[data-user-template]")
-// const userCardContainer = document.querySelector("[data-user-cards-container]")
-// const searchInput = document.querySelector("[data-search]")
+    const userCardTemplate = document.querySelector("[data-user-template]")
+const userCardContainer = document.querySelector("[data-user-cards-container]")
+const searchInput = document.querySelector("[data-search]")
 
-// let users = []
+let users = []
 
-// searchInput.addEventListener("input", e => {
-//   const value = e.target.value.toLowerCase()
-//   users.forEach(user => {
-//     const isVisible =
-//       user.name.toLowerCase().includes(value) ||
-//       user..toLowerCase().includes(value)
-//     user.element.classList.toggle("hide", !isVisible)
-//   })
-// })
+searchInput.addEventListener("input", e => {
+  const value = e.target.value.toLowerCase()
+  users.forEach(user => {
+    const isVisible =
+      user.name.toLowerCase().includes(value) ||
+      user..toLowerCase().includes(value)
+    user.element.classList.toggle("hide", !isVisible)
+  })
+})
 
-// fetch("http://hp-api.herokuapp.com/api/characters")
-//   .then(res => res.json())
-//   .then(data => {
-//     users = data.map(user => {
-//       const card = userCardTemplate.content.cloneNode(true).children[0]
-//       const header = card.querySelector("[data-header]")
-//       const body = card.querySelector("[data-body]")
-//       header.textContent = user.name
-//       body.textContent = user.email
-//       userCardContainer.append(card)
-//       return {name: user.name, house: user.house, image: user.image,  element: card }
-//     })
-//   })
+fetch("http://hp-api.herokuapp.com/api/characters")
+  .then(res => res.json())
+  .then(data => {
+    users = data.map(user => {
+      const card = userCardTemplate.content.cloneNode(true).children[0]
+      const header = card.querySelector("[data-header]")
+      const body = card.querySelector("[data-body]")
+      header.textContent = user.name
+      body.textContent = user.email
+      userCardContainer.append(card)
+      return {name: user.name, house: user.house, image: user.image,  element: card }
+    })
+  })
 
